ee7caaf82cfb2f5bb2296054a1209cc26f74e808
Cheung Ka Wai <zhtmike@gmail.com>
2026-07-20 22:49:27 +0800

    Add: string-encode-and-decode - submission-9

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-9.py
@@ -0,0 +1,28 @@
+import hashlib
+
+class Solution:
+    # a 7 digit hash is pretty enough, < 1e-9 prob to have collision
+    hash_ = hashlib.sha256(b"0").hexdigest()[:5]
+
+    def encode(self, strs: List[str]) -> str:
+        if len(strs) == 0:
+            return ""
+        if len(strs) == 1:
+            if strs[0] == "":
+                return self.hash_
+            return strs[0]
+        if strs == ["", ""]:
+            return self.hash_ + self.hash_
+        strs = self.hash_.join(strs)
+        return strs
+
+    def decode(self, s: str) -> List[str]:
+        if len(s) == 0:
+            return []
+        if s == self.hash_:
+            return [""]
+        if len(s) == 1:
+            return [s]
+        if s == self.hash_ + self.hash_:
+            return ["", ""]
+        return s.split(self.hash_)
