583bc3b2179615196539353fd87bc3769c58f5ca
Cheung Ka Wai <zhtmike@gmail.com>
2026-08-13 23:09:43 +0800

    Add: buy-and-sell-crypto - submission-11

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-11.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) < 2:
+            return 0
+
+        buy_ind, sell_ind = 0, 1
+        maybe_buy_ind = 0
+
+        for i in range(1, len(prices)):
+            if prices[i] - prices[maybe_buy_ind] >= prices[sell_ind] - prices[buy_ind]:
+                sell_ind, buy_ind = i, maybe_buy_ind
+            if prices[i] < prices[maybe_buy_ind]:
+                maybe_buy_ind = i
+
+        return max(0, prices[sell_ind] - prices[buy_ind])
